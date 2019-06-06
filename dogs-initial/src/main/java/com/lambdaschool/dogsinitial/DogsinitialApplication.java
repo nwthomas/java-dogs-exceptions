90366158eca03fddbad55d93b80513248bfb67c6
Nathan Thomas <nwthomas@me.com>
2019-06-06 00:40:05 -0700

    Finish building out error messages for server

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.java
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/DogsinitialApplication.java
@@ -2,7 +2,11 @@ package com.lambdaschool.dogsinitial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class DogsinitialApplication
 {
@@ -11,8 +15,11 @@ public class DogsinitialApplication
     public static void main(String[] args)
     {
         ourDogList = new DogList();
-        SpringApplication.run(DogsinitialApplication.class, args);
-    }
+        ApplicationContext ctx = SpringApplication.run(DogsinitialApplication.class, args);
 
+        DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+    }
 }
 
