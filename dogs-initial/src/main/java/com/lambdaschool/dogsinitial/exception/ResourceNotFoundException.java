b3c3ca2848035fa6c473165e73815cd3ca820619
Nathan Thomas <nwthomas@me.com>
2019-06-06 00:02:58 -0700

    Build out ErrorDetail.java class for errors

---
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/exception/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.dogsinitial.exception;
+
+public class ResourceNotFoundException
+{
+}
---
+++ dogs-initial/src/main/java/com/lambdaschool/dogsinitial/handler/RestExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.lambdaschool.dogsinitial.handler;
+
+public class RestExceptionHandler
+{
+}
